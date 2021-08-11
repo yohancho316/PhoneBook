ad6fdd1b772cf745a06e7126c02b823206288ad8
John Cho <yohancho316@gmail.com>
2021-08-10 19:16:03 -0700

    Created Database Code

--- database.py
+++ database.py
@@ -1,36 +1,23 @@
+###################### Import Statements ######################
 import sqlite3
 
-connection = sqlite3.connect('john_phone_book.db')
+###################### Stored SQL Queries ######################
+CREATE_TABLE_QUERY = 'CREATE TABLE IF NOT EXISTS contacts(name TEXT,phone_number TEXT, relationship TEXT);'
+
+####################### Python Code ########################
+
+# Create the Database & SQLite3 Database File
+connection = sqlite3.connect('address_book.db')
 # by default the connection grabs columns and stores them in a tuple which can be stored in the cursor
 # you can change connection to grab columns and store them as a dictionary in the cursor.
 connection.row_factory = sqlite3.Row
 
-
-welcome = "Welcome to John's Phone Book"
-
-menu = """
-Please choose a selection.
-
-1. Add new entry.
-2. View all entries.
-3. Quit.
-
-Enter your selection:
-"""
-
 def create_table():
-  with connection:
-    connection.execute('CREATE TABLE IF NOT EXISTS users (name TEXT, phone_number TEXT, age INTEGER);')
+  connection.execute(CREATE_TABLE_QUERY)
 
 def add_entry(name,phone_number,age):
   with connection:
     connection.execute('INSERT INTO users VALUES(?,?,?);',(name,phone_number,age))
-
-def prompt_entry():
-  name = input("Enter Name to Record: ")
-  phone_number = input("Enter Phone Number: ")
-  age = input("Enter age: ")
-  add_entry(name,phone_number,age)
 
 def get_entries():
   # Method 1
@@ -40,24 +27,11 @@ def get_entries():
   # cursor.fetchone() # Fetch the first row and move the cursor to the next row
   return cursor.fetchall() # Fetches all of the results from the cursor and puts them in a list.
 
-def view_entries(entries):
-  # Prints row by row
-  for entry in entries:
-    print(f"{entry['name']}\n{entry['phone_number']}\n{entry['age']}\n\n")
 
 def close_connection():
   connection.close()
 
-print(welcome)
-create_table()
 
-while(user_input := input(menu)) != "3":
-  if user_input == "1":
-    prompt_entry()
-  elif user_input == "2":
-    view_entries(get_entries())
-  else:
-    print("Invalid option, please try again!")
 
-close_connection()
+
 
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+###################### Import Statements ######################
 import tkinter as tk
 from tkinter import ttk
 import sqlite3
+import database as database
 
+######################### TKinter Code ########################
 class AddressBook(tk.Tk):
   def __init__(self):
     super().__init__()
@@ -11,20 +14,26 @@ class AddressBook(tk.Tk):
     self.columnconfigure(0,weight=1)
     self.rowconfigure(0,weight=1)
 
+    # Create SQLite3 Database & DB File
+    database.create_table()
+
+    # Create Container Frame
     container = ttk.Frame(self)
     container.grid()
 
+    # Create Entry Frame
     entry_frame = Entry(container)
     entry_frame.grid(row=0,column=0,sticky='W')
 
+# Entry Frame Class
 class Entry(ttk.Frame):
   def __init__(self,parent):
     super().__init__(parent)
     self.name = tk.StringVar()
     self.phone_number = tk.StringVar()
-    self.Relationship = tk.StringVar()
+    self.relationship = tk.StringVar()
 
-    # Labels
+    # Label Widgets
     name_label = ttk.Label(self,text='Name: ')
     phone_label = ttk.Label(self,text='Phone: ')
     relationship_label = ttk.Label(self,text='Relationship: ')
@@ -33,6 +42,32 @@ class Entry(ttk.Frame):
     phone_label.grid(row=1,column=0,sticky='W')
     relationship_label.grid(row=2,column=0,sticky='W')
 
+    # Entry Widgets
+    self.name_entry = ttk.Entry(self,textvariable=self.name)
+    self.phone_number_entry = ttk.Entry(self,textvariable=self.phone_number)
+    self.relationship_entry = ttk.Entry(self,textvariable=self.relationship)
+
+    self.name_entry.grid(row=0,column=1,sticky='W')
+    self.phone_number_entry.grid(row=1,column=1,sticky='W')
+    self.relationship_entry.grid(row=2,column=1,sticky='W')
+
+    # Button Widgets
+    clear_button = ttk.Button(self,text='Clear',command=self.clear_entry_fields)
+    add_button = ttk.Button(self,text='Add',command=self.print_entry)
+
+    clear_button.grid(row=3,column=0,columnspan=1,sticky='EW')
+    add_button.grid(row=3,column=1,columnspan=1,sticky='EW')
+
+  # Clear Entry Fields
+  def clear_entry_fields(self):
+    self.name_entry.delete(0,'end')
+    self.phone_number_entry.delete(0,'end')
+    self.relationship_entry.delete(0,'end')
+
+  def print_entry(self):
+    print(self.name.get(),self.phone_number.get(),self.relationship.get())
+
+############################# Python Code #######################
 
 phonebook = AddressBook()
 phonebook.mainloop()
